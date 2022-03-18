b927f2975007eee0cdf4ee975ac2a6b24f9ca68d
Tran Ngoc Phuoc <phuoc04012000@gmail.com>
2022-03-18 10:05:01 +0700

    [feature][register_doctor_role]admin list request, user request role to admin, admin accept role

---
+++ app/Http/Controllers/Api/Auth/RegisterController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Exception;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class RegisterController extends Controller
+{
+    /**
+     * Get the list of register user who request to become doctor role
+     *
+     * @return JsonResponse
+     */
+    public function getListRegisterDoctorRoles(): JsonResponse
+    {
+        try {
+            $registerUsers = DB::table('register_doctor_role_drafts')
+                ->where('is_accept', 'false')
+                ->paginate('10');
+            return response()->json($registerUsers);
+        } catch (Exception $exception) {
+            return response()->json($exception->getMessage(), 500);
+        }
+    }
+
+    /**
+     * Register doctor role
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function registerWithRoleDoctor(Request $request): JsonResponse
+    {
+        // TODO:: taoj request validate
+        try {
+            $user = User::findOrFail($request->user()->id);
+            DB::table('register_doctor_role_drafts')->insert([
+                'user_id' => $user->id,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+            return response()->json('Register success');
+        } catch (Exception $exception) {
+            return response()->json($exception->getMessage(), 500);
+        }
+    }
+
+    /**
+     * Admin accept request register doctor role from normal user
+     *
+     * @param $registerUserID
+     * @return JsonResponse
+     */
+    public function acceptRegisterDoctorRole($registerUserID): JsonResponse
+    {
+//        TODO:: tao request validate
+        try {
+            $registerUser = DB::table('register_doctor_role_drafts')
+                ->where('user_id', $registerUserID)
+                ->where('is_accept', false)
+                ->first();
+
+            if (!is_null($registerUser)) {
+                DB::table('register_doctor_role_drafts')
+                    ->where('user_id', $registerUserID)
+                    ->update(['is_accept' => true]);
+                $user = User::findOrFail($registerUserID);
+                $user->assignRole('doctor'); // Assign doctor role
+                $user->givePermissionTo('create a post', 'update a post', 'delete a post');
+                return response()->json('Accept success');
+            } else {
+                return response()->json('User not found or not have request register doctor role!', 404);
+            }
+        } catch (Exception $exception) {
+            return response()->json($exception->getMessage(), 500);
+        }
+    }
+}
--- routes/Apis/Auth/auth.php
+++ routes/Apis/Auth/auth.php
@@ -2,29 +2,48 @@
 
 use App\Http\Controllers\Api\Auth\AuthController;
 use App\Http\Controllers\Api\Auth\GoogleAuthController;
+use App\Http\Controllers\Api\Auth\RegisterController;
 use App\Http\Controllers\Api\Auth\ResetPasswordController;
 use App\Http\Controllers\Api\Auth\VerifyAccountController;
 use Illuminate\Support\Facades\Route;
 
+/**
+ * Authentication to system
+ */
 Route::controller(AuthController::class)->group(function () {
     Route::post('/login', 'login');
     Route::post('/register', 'register');
     Route::post('/logout', 'logout')->middleware(['auth:sanctum']);
 });
 
-// Login with Google account
+/**
+ * Login with Google account
+ */
 Route::post('/login/google', [GoogleAuthController::class, 'login']);
 
-// Reset password
+/**
+ * Reset password
+ */
 Route::controller(ResetPasswordController::class)->group(function () {
     Route::post('/forgot-password', 'forgotPassword');
     Route::put('/reset-password', 'resetPassword');
 });
 
 
-// Verify account
+/**
+ * Verify account
+ */
 Route::controller(VerifyAccountController::class)->group(function () {
     Route::post('/email/verification-notification', 'resendVerifyEmail');
     Route::put('/verify-account', 'verifyEmail');
 });
 
+/**
+ * Register doctor role
+ */
+Route::controller(RegisterController::class)->middleware(['auth:sanctum'])->group(function () {
+    Route::get('/register/doctor-role', 'getListRegisterDoctorRoles'); //TODO Middle is admin here
+    Route::post('/register/doctor-role', 'registerWithRoleDoctor'); // TODO:: add check is not doctor
+    // TODO make middleware is admin, is doctor, is normal user
+    Route::put('/register/doctor-role/{registerUserID}', 'acceptRegisterDoctorRole'); //TODO:: add middle admin here
+});
