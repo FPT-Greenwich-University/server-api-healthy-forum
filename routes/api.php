805faaee78ea538919e01c6c81d7327a7ba14a7a
Tran Ngoc Phuoc <phuoc04012000@gmail.com>
2022-05-23 20:51:17 +0700

    [feature][admin_manager_permission]

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,6 @@
 <?php
 
-use App\Http\Controllers\Api\Admins\Categories\CategoryController;
-use App\Http\Controllers\Api\Admins\Posts\PostController;
-use App\Http\Controllers\Api\Admins\Users\UserController;
+
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -21,38 +19,3 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return response()->json(User::with('roles')->findOrFail($request->user()->id));
 });
-
-/*
- * Admin handle posts
- */
-Route::prefix('/admins')
-    ->controller(PostController::class)
-    ->middleware(['role:admin', 'auth:sanctum'])
-    ->group(function () {
-        Route::get('/posts/not-published', 'getPostsIsNotPublished'); // Get posts where are not published
-        Route::get('/posts/{postID}', 'show');
-        Route::put('/posts/{postID}/publish', 'acceptPublishPost'); // Published post by update status => true for the post
-    });
-
-/*
- * Admin handle posts
- */
-Route::prefix('/admins')
-    ->controller(CategoryController::class)
-    ->middleware(['role:admin', 'auth:sanctum'])
-    ->group(function () {
-        Route::post('/categories', 'store');
-        Route::put('/categories/{categoryID}', 'update');
-        Route::delete('/categories/{categoryID}', 'destroy');
-    });
-
-/**
- * Admin manager user
- */
-Route::prefix('/admins')
-    ->middleware(['role:admin', 'auth:sanctum'])
-    ->controller(UserController::class)
-    ->group(function () {
-        Route::get('/users', 'index'); // List all users
-        Route::get('/roles', 'getRoles');
-    });
