12d7adac56e0416b685af6bd836b31ff381fa6d6
ngocphuocha <phuoc04012000@gmail.com>
2022-07-17 04:47:42 +0700

    Refactor PublicPostController.php

--- app/Http/Controllers/Api/Public/Posts/PublicPostController.php
+++ app/Http/Controllers/Api/Public/Posts/PublicPostController.php
@@ -5,30 +5,25 @@ namespace App\Http\Controllers\Api\Public\Posts;
 use App\Http\Controllers\Controller;
 use App\Repositories\Interfaces\ICategoryRepository;
 use App\Repositories\Interfaces\IPostRepository;
-use App\Repositories\Interfaces\ITagRepository;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 
 class PublicPostController extends Controller
 {
     private readonly IPostRepository $postRepos;
-    private readonly ITagRepository $tagRepos;
     private readonly ICategoryRepository $categoryRepos;
 
-    public function __construct(IPostRepository $IPostRepository, ITagRepository $ITagRepository, ICategoryRepository $categoryRepository)
+    public function __construct(IPostRepository $IPostRepository, ICategoryRepository $categoryRepository)
     {
         $this->postRepos = $IPostRepository;
-        $this->tagRepos = $ITagRepository;
         $this->categoryRepos = $categoryRepository;
     }
 
     /**
      * Display all posts of the resources.
      *
-     * @param Request $request
      * @return JsonResponse
      */
-    public function index(Request $request): JsonResponse
+    public function index(): JsonResponse
     {
         $perPage = 10; // the total post in one page
         return response()->json($this->postRepos->filterPosts($perPage));
